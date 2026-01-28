e840ef9f2acfae2c8dc0bc68e69484a506902d74
Dipratna Mahire <dipratnamahire2222@gmail.com>
2026-01-28 16:45:12 +0530

    pl

--- src/Components/BbaAdmission/BbaAdmissionInfra.jsx
+++ src/Components/BbaAdmission/BbaAdmissionInfra.jsx
@@ -273,6 +273,7 @@ export default function CampusFacilities() {
                     </p>
                   </div>
                 </div>
+
               </div>
             ))}
           </div>
